c41bd7754aa73977a63078f77ed3f7ef20b24837
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-10-25 09:54:50 -0700

    add halloween version
    Add Halloween version of Fart.css library.

--- main.ts
+++ main.ts
@@ -1,12 +1,22 @@
-import { serveDir, serveFile } from "@std/http";
+import { serveDir } from "@std/http";
+import { appendHalloweenCSS } from "./halloween.ts";
 
 Deno.serve(async (request) => {
   const url = new URL(request.url);
   if (
-    url.pathname === "/" &&
+    (url.pathname === "/" ||
+      url.pathname === "/fart.css") &&
     request.headers.get("Accept")?.includes("text/css")
   ) {
-    return await serveFile(request, "fart.css");
+    const fartCSS = appendHalloweenCSS(await Deno.readTextFile("fart.css"));
+    return new Response(
+      fartCSS,
+      {
+        headers: new Headers({
+          "Content-Type": "text/css",
+        }),
+      },
+    );
   }
 
   return await serveDir(request);
