bce105f9807a1970563ad2b1a7f980eb8e0a715e
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-10-27 22:35:34 -0700

    skip `appendCSS` on `?minimal` url query parameter

--- main.ts
+++ main.ts
@@ -2,33 +2,34 @@ import { serveDir } from "@std/http";
 import { checkHalloweenWeek } from "./lib/css/halloween.ts";
 import { appendCSS } from "./lib/css/append-css.ts";
 
-Deno.serve(async (request) => {
-  const url = new URL(request.url);
-  if (
-    (url.pathname === "/" ||
-      url.pathname === "/fart.css") &&
-    request.headers.get("Accept")?.includes("text/css")
-  ) {
-    const fartCSS = appendCSS(
-      await Deno.readTextFile("fart.css"),
-      [
+if (import.meta.main) {
+  Deno.serve(async (request) => {
+    const url = new URL(request.url);
+    if (
+      (url.pathname === "/" ||
+        url.pathname === "/fart.css") &&
+      request.headers.get("Accept")?.includes("text/css")
+    ) {
+      const minFartCSS = await Deno.readTextFile("fart.css");
+      const fartCSS = url.searchParams.has("minimal") ? minFartCSS : appendCSS(
+        minFartCSS,
+        [
+          {
+            css: '@import "halloween.css";\n',
+            test: (date) =>
+              checkHalloweenWeek(date) || url.searchParams.has("halloween"),
+          },
+        ],
+      );
+
+      return new Response(
+        fartCSS,
         {
-          css: '@import "halloween.css";\n',
-          test: (date) =>
-            checkHalloweenWeek(date) || url.searchParams.has("halloween"),
+          headers: new Headers({ "Content-Type": "text/css" }),
         },
-      ],
-    );
+      );
+    }
 
-    return new Response(
-      fartCSS,
-      {
-        headers: new Headers({
-          "Content-Type": "text/css",
-        }),
-      },
-    );
-  }
-
-  return await serveDir(request);
-});
+    return await serveDir(request);
+  });
+}
