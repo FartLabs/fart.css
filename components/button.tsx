f949efadd95b42e58a0b0d8fc137e6f77c66ee5a
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2025-05-11 16:26:50 -0700

    Add Button property "as"
    Reference: https://v2.chakra-ui.com/community/recipes/as-prop

--- components/button.tsx
+++ components/button.tsx
@@ -1,26 +1,40 @@
+import { A } from "@fartlabs/htx/a";
 import { BUTTON } from "@fartlabs/htx/button";
 
 /**
  * ButtonProps are the properties of the Button component.
  */
-export interface ButtonProps
-  extends Exclude<Parameters<typeof BUTTON>[0], undefined> {
-}
+export type ButtonProps<T extends "a" | "button"> = T extends "a"
+  ? Parameters<typeof A>[0] & { as?: T }
+  : T extends "button" ? Parameters<typeof BUTTON>[0] & { as: T }
+  : never;
 
 /**
  * Button is a button element with a Fart.css style.
  *
  * @see https://css.fart.tools#buttons
  */
-export function Button(props: ButtonProps): string {
-  return (
-    <BUTTON
-      {...props}
-      class={`fart-button${props.class ? ` ${props.class}` : ""}`}
-    >
-      {props.children}
-    </BUTTON>
-  );
+export function Button<T extends "a" | "button" = "a">(
+  props: ButtonProps<T>,
+): string {
+  const modifiedProps: ButtonProps<T> = {
+    ...props,
+    class: `fart-button${props.class ? ` ${props.class}` : ""}`,
+  };
+
+  switch (props.as ?? "a") {
+    case "a": {
+      return <A {...modifiedProps} />;
+    }
+
+    case "button": {
+      return <BUTTON {...modifiedProps} />;
+    }
+
+    default: {
+      throw new Error("Invalid button type");
+    }
+  }
 }
 
-export { BUTTON };
+export { A, BUTTON };
